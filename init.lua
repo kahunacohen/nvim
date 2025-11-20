d613ee1b74538080509bfad88e92e0e92ccba43f
Aharon Cohen <aharonc@matav.org.il>
2025-11-20 12:26:34 +0200

    add auto go format

--- init.lua
+++ init.lua
@@ -190,6 +190,14 @@ for name, config in pairs(servers) do
   vim.lsp.enable(name)
 end
 
+-- Format Go files on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+    vim.lsp.buf.format({ async = false })
+  end,
+})
+
 ------------------------------------------------------------
 -- nvim-cmp
 ------------------------------------------------------------
